3f127574c9f703bdbe0df4d1bff1bc8f3fa90eca
Vova-code <vdoche@live.fr>
2023-07-07 09:55:15 +0200

    Log env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,9 @@ import process from "process";
 
 const LOCAL_API_URL: string = "http://localhost:8000/api/v1"
 export const getApiURL = (): string => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.API_URL);
+
     if (process.env.NODE_ENV === "development") {
         return  LOCAL_API_URL
     }
