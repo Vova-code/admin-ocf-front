28f1e22e16152f57fa7f1fc553c1b63c5f54a549
Vova-code <vdoche@live.fr>
2023-07-07 10:22:32 +0200

    Fix env variables

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,10 +3,10 @@ import process from "process";
 const LOCAL_API_URL: string = "http://localhost:8000/api/v1"
 export const getApiURL = (): string => {
     console.log(process.env.NODE_ENV);
-    console.log(process.env.API_URL);
+    console.log(process.env.NEXT_PUBLIC_API_URL);
 
     if (process.env.NODE_ENV === "development") {
         return  LOCAL_API_URL
     }
-    return process.env.API_URL
+    return process.env.NEXT_PUBLIC_API_URL
 }
